4e0f0452c5c805147b58aa8a2bb7421d97466c89
wissal bejaoui <wissal.bejaoui@esprit.com>
2023-02-16 01:45:39 +0100

    integration consultation

--- src/Controller/IntegrationController.php
+++ src/Controller/IntegrationController.php
@@ -11,16 +11,18 @@ class IntegrationController extends AbstractController
     #[Route('/integration', name: 'app_integration')]
     public function index(): Response
     {
-        return $this->render('Front-Office/Landing.html.twig', [
-            'controller_name' => 'IntegrationController',
-        ]);
+        return $this->render('Front-Office/Landing.html.twig');
     }
 
     #[Route('/admin', name: 'app_integration2')]
     public function admin(): Response
     {
-        return $this->render('Back-Office/DashboardAdmin.html.twig', [
-            'controller_name' => 'IntegrationController',
-        ]);
+        return $this->render('Back-Office/DashboardAdmin.html.twig');
+    }
+
+    #[Route('/consultation', name: 'app_consult')]
+    public function dashboard(): Response
+    {
+        return $this->render('Front-Office/consultation.html.twig');
     }
 }
