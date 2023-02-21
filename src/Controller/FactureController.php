7e318bbd81d50088756765545d6d157c7cc078c3
unknown <feryel.ouerfelli@esprit.tn>
2023-02-21 23:36:40 +0100

    Uploadimage

--- src/Controller/FactureController.php
+++ src/Controller/FactureController.php
@@ -9,6 +9,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+
 
 #[Route('/facture')]
 class FactureController extends AbstractController
@@ -22,13 +26,42 @@ class FactureController extends AbstractController
     }
 
     #[Route('/new', name: 'app_facture_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, FactureRepository $factureRepository): Response
+    public function new(Request $request, FactureRepository $factureRepository, SluggerInterface $slugger): Response
     {
         $facture = new Facture();
         $form = $this->createForm(FactureType::class, $facture);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $image = $form->get('image')->getData();
+
+            // this condition is needed because the 'brochure' field is not required
+            // so the PDF file must be processed only when a file is uploaded
+            if ($image) {
+                $originalFilename = pathinfo($image->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$image->guessExtension();
+
+                // Move the file to the directory where brochures are stored
+                try {
+                    $image->move(
+                        $this->getParameter('image_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+
+                // updates the 'brochureFilename' property to store the PDF file name
+                // instead of its contents
+                $facture->setImageSignature($newFilename);
+            }
+
+
+
+
+
             $factureRepository->save($facture, true);
 
             return $this->redirectToRoute('app_facture_index', [], Response::HTTP_SEE_OTHER);
--- src/Form/FactureType.php
+++ src/Form/FactureType.php
@@ -9,8 +9,12 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\NotNull;
 use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\File;
+
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+
 
 
 
@@ -30,7 +34,33 @@ class FactureType extends AbstractType
                 'required' => true
             ])           
             ->add('ordonnance')
-            ->add('image_signature')
+            ->add('image', FileType::class, [
+                'label' => 'Ajouter votre image de signature(JPG, GIF ou PNG autorisés. Taille maximale de 2 Mo)                ',
+
+                // unmapped means that this field is not associated to any entity property
+                'mapped' => false,
+
+                // make it optional so you don't have to re-upload the PDF file
+                // every time you edit the Product details
+                'required' => false,
+
+                // unmapped fields can't define their validation using annotations
+                // in the associated entity, so you can use the PHP constraint classes
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'image/jpg',
+                            'image/png',
+                            'image/gif',
+                            'image/jpeg',
+
+
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid image',
+                    ])
+                ],
+            ])
             ->add('montant')
 
         ;
